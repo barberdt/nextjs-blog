343123d6b8dff0679e578868eefb0a8216b019be
Davis Barber <barberdt@gmail.com>
2020-10-19 13:26:22 -0400

    remove duplicate provesource

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,6 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
-        <script
-          dangerouslySetInnerHTML={{ __html: `
-            !function(o,i){window.provesrc&&window.console&&console.error&&console.error("ProveSource is included twice in this page."),provesrc=window.provesrc={dq:[],display:function(){this.dq.push(arguments)}},o._provesrcAsyncInit=function(){provesrc.init({apiKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SWQiOiI1Zjg5ZWQyOWI3ZTYzMzVkYmVkMDliZTIiLCJpYXQiOjE2MDI4NzQ2NjV9.R8VX762KTqwD320aiYxVDh8JUlHiBij4Yl5tlK9LpUI",v:"0.0.4"})};var r=i.createElement("script");r.type="text/javascript",r.async=!0,r["ch"+"ar"+"set"]="UTF-8",r.src="https://cdn.provesrc.com/provesrc.js";var e=i.getElementsByTagName("script")[0];e.parentNode.insertBefore(r,e)}(window,document);
-          `}}
-        ></script>
-        <script type="text/javascript" src="https://load.fomo.com/api/v1/a7NlgXCbR-8n2dOpROMK-g/load.js" async></script>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
